aa1f88df4f9c8b74a119c6ad625676c5b356efde
shono <s.shosho1210@gmail.com>
2017-06-22 22:14:11 +0900

    first commit

---
+++ src/App.vue
@@ -0,0 +1,69 @@
+<template>
+  <div>
+    <h2>hello world</h2>
+
+    <input type="text" v-model="name">
+    <p>Hello, {{name}}!</p>
+    <p>{{name.length}}/20</p>
+    <p v-if="name.length > 20">20文字以内で入力してください</p>
+
+    <my-profile></my-profile>
+
+    <p>{{msg}}</p>
+
+    <a @click="sample" class="btn">click</a>
+  </div>
+
+</template>
+
+<script>
+
+import Profile from "./Profile.vue"
+
+export default {
+  name: 'app',
+  data () {
+    return {
+      msg: 'Welcome to Your Vue.js App',
+      name: 'John Smith'
+    }
+  },
+  components: {
+    "my-profile": Profile
+  },
+  methods: {
+      sample(){
+          this.msg = "hello, my sample app"
+      }
+  }
+}
+</script>
+
+<style>
+#app {
+  font-family: 'Avenir', Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+
+h1, h2 {
+  font-weight: normal;
+}
+
+ul {
+  list-style-type: none;
+  padding: 0;
+}
+
+li {
+  display: inline-block;
+  margin: 0 10px;
+}
+
+a {
+  color: #42b983;
+}
+</style>
---
+++ src/Profile.vue
@@ -0,0 +1,17 @@
+<template>
+    <div>
+        <p>name: {{user}}</p>
+        <p>age: 18</p>
+    </div>
+</template>
+
+<script>
+    export default{
+        name: "profile",
+        data(){
+            return {
+                user: "John"
+            }
+        }
+    }
+</script>
